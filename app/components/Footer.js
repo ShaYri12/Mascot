8e5f0af17f2d514ba8d2acc4524eb012eb1baf82
ShaYri12 <sharjeelb60@gmail.com>
2024-07-12 20:46:40 +0500

    mascot

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex px-8 text-black">
+      <p>© 2024 trymascot.com- All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center w-full">
+      <div className="flex items-center p-[35px]">
+        <img
+          className="w-[173px] h-[25px]"
+          src="/assets/mascotlogo.avif"
+          alt=""
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
